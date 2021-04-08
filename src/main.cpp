f8a567899b76168b1543b2b8d2b7e1f91d7b84c8
Jareth <31932204+JustJarethB@users.noreply.github.com>
2021-04-09 00:20:10 +0100

    oop hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
 
-#define INTERNAL_LED 13
-bool ledState = false;
+class Component {
+  int pin;
+  public:
+  Component(int pin) {
+    this->pin = pin;
+  }
+  int getPin() {
+    return pin;
+  }
+};
+
+class DigitalOutput : Component {
+  protected:
+  bool value;
+  public:
+  DigitalOutput(int pin, bool initialValue) : Component{pin} {
+    pinMode(getPin(), OUTPUT);
+    value = initialValue;
+    updateHardware();
+  }
+  void setValue(bool value) {
+    this->value = value;
+    updateHardware();
+  }
+  bool getValue() {
+    return value;
+  }
+
+  protected:
+  void updateHardware() {
+    digitalWrite(getPin(), value);
+  }
+};
+
+DigitalOutput internalLED(13,false);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello World");
-  pinMode(INTERNAL_LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(INTERNAL_LED, ledState);
-  ledState = !ledState;
+  internalLED.setValue(!internalLED.getValue());
   delay(1000);
 }
