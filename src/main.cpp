077482648756edb199d11b508ebcdfa7fcfd31ae
Jareth <31932204+JustJarethB@users.noreply.github.com>
2021-04-10 19:38:44 +0100

    start flash, refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,24 @@
 #include <DigitalOutput.h>
 #include <DigitalInput.h>
 
-
+// INPUTS
+Button button(12);
+// OUTPUTS
 DigitalOutput internalLED(13,false);
 
 
-Button button(12);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello World");
+  for (__int8_t i = 0; i < 5; i++)
+  {
+    internalLED.on();
+    delay(100);
+    internalLED.off();
+    delay(100);
+  }
 }
 
 void loop() {
